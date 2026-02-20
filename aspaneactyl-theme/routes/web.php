1b734024094a72ab452121cd282b0aaeaae85762
HanzsX <130972657+HanzsX@users.noreply.github.com>
2026-02-21 00:14:49 +0800

    Delete aspaneactyl-theme directory

--- aspaneactyl-theme/routes/web.php
+++
@@ -1,4 +0,0 @@
-Route::group(['middleware' => ['auth']], function() {
-    Route::get('/admin/theme-settings', [ThemeSettingsController::class, 'getSettings']);
-    Route::post('/admin/theme-settings', [ThemeSettingsController::class, 'updateSettings']);
-});
